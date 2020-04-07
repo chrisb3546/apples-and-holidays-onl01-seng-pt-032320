6ba705f07a3db88ae817b052a488f969a3724add
chris brady <chrisb3546@yahoo.com>
2020-04-07 13:01:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,9 +60,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts "#{season.to_s.capitalize}:"
-  holiday.each do |event,supplies|
-  puts "#{event.to_s.split("_").map {|x|x.capitalize}.join(" ") }: #{supplies.join(", ")}"
+puts "#{season.to_s.capitalize}:"
+holiday.each do |event,supplies|
+puts "#{event.to_s.split("_").map {|x|x.capitalize}.join(" ") }: #{supplies.join(", ")}"
 end 
 end
 end 
